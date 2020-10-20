288da2aebbbd6e96fbb68fc6db38f3aa1a21df65
BhavaniShanker999 <bhavanigloled@gmail.com>
2020-10-21 02:55:14 +0530

    first commit

--- accounts/models.py
+++ accounts/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Customer(models.Model):
     name=models.CharField(max_length=200,null=True,)
-    phone = models.CharField( null=True,)
+    phone = models.IntegerField(null=True,)
     email = models.EmailField(max_length=50, null=True,)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
    
